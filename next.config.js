5912e6d7b6f1f8b3ea9c0de65e77bbf44aef2ca4
katarinasupe <supe.katarina@gmail.com>
2023-08-01 16:31:15 +0200

    Add example usage

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 const withNextra = require('nextra')({
   theme: 'nextra-theme-docs',
   themeConfig: './theme.config.tsx',
+  latex: true,
+  defaultShowCopyCode: true
 })
 
 module.exports = withNextra()
