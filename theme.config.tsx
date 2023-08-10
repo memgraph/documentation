69f41d2eebb6e55ec265e0353dfa14d45446a974
Kruno Golubic <46486712+kgolubic@users.noreply.github.com>
2023-08-10 14:08:08 +0200

    Add fav icon (#16)

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,17 +2,22 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  logo: <span>Memgraph Documentation</span>,
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    link: 'https://github.com/memgraph/memgraph',
   },
   chat: {
-    link: 'https://discord.com',
+    link: 'https://discord.gg/memgraph',
   },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  docsRepositoryBase: 'https://github.com/memgraph/memgraph',
   footer: {
-    text: 'Nextra Docs Template',
+    text: 'Memgraph',
   },
+  head: (
+    <>
+      <link rel="icon" href="/favicon.png" />
+    </>
+  )
 }
 
 export default config
