0b6a7e3f9eb4f76096ea19f935a4df64432c7ffe
kgolubic <kgolubic@gmail.com>
2023-09-11 16:42:26 +0200

    Add footer

--- components/Footer.js
+++ components/Footer.js
@@ -1,41 +1,47 @@
 import React from 'react';
+import Image from 'next/image';
 
-const Footer = () => {
-  return (
-    <footer className="nx-bg-gray-100 nx-pb-[env(safe-area-inset-bottom)] dark:nx-bg-neutral-900 print:nx-bg-transparent">
-      <div style={{ maxWidth: '1200px', margin: '0 auto', display: 'flex', justifyContent: 'space-between' }}>
-      <div>
-          <strong>Documentation</strong>
-          <ul>
-            <li><a href="/docs/memgraph">Memgraph</a></li>
-            <li><a href="/docs/memgraph-lab">Memgraph Lab</a></li>
-            <li><a href="/docs/memgraph-cloud">Memgraph Cloud</a></li>
-            <li><a href="/docs/cypher-manual">Cypher manual</a></li>
-            <li><a href="/docs/mage">MAGE</a></li>
-            <li><a href="/docs/gqlalchemy">GQLAlchemy</a></li>
-          </ul>
+const ExternalLink = ({ href, children }) => (
+  <a href={href} target="_blank" rel="noopener noreferrer" className="external-link">
+      <span className="external-link-text">{children}</span>
+      <Image src="/docs/external-link.svg" alt="External Link" width={16} height={17} />
+  </a>
+);
+
+const Footer = () => (
+  <footer className="footer-container">
+        <div>
+          <img src="/docs/memgraph-logo-footer.svg" alt="Memgraph Logo" width="162" height="55" />
         </div>
         <div>
-          <strong>Community</strong>
-          <ul>
-            <li><a href="https://discord.gg/memgraph" target="_blank" rel="noopener noreferrer">Discord</a></li>
-            <li><a href="https://stackoverflow.com/questions/tagged/memgraphdb" target="_blank" rel="noopener noreferrer">Stack Overflow</a></li>
-            <li><a href="https://twitter.com/memgraphdb" target="_blank" rel="noopener noreferrer">Twitter</a></li>
-          </ul>
+            <ul className="column-text">
+                <li><span className="column-title">Documentation</span ></li>
+                <li><a href="/docs/getting-started">Get started</a></li>
+                <li><a href="/docs/data-migration">Migrate data</a></li>
+                <li><a href="/docs/querying">Query data</a></li>
+                <li><a href="/docs/custom-query-modules">Create an app</a></li>
+                <li><a href="/docs/data-visualization">Visualize data</a></li>
+                <li><a href="/docs/advanced-algorithms">Use advanced algorithms</a></li>
+            </ul>
         </div>
         <div>
-          <strong>More</strong>
-          <ul>
-            <li><a href="https://memgraph.com/cloud" target="_blank">Memgraph Cloud</a></li>
-            <li><a href="https://playground.memgraph.com" target="_blank">Memgraph Playground</a></li>
-            <li><a href="https://github.com/memgraph/memgraph" target="_blank">GitHub</a></li>
-            <li><a href="https://www.youtube.com/channel/UCZ3HOJvHGxtQ_JHxOselBYg" target="_blank">YouTube</a></li>
-          </ul>
+            <ul className="column-text">
+                <li><span className="column-title">Community</span ></li>
+                <li><ExternalLink href="https://discord.gg/memgraph">Discord</ExternalLink></li>
+                <li><ExternalLink href="https://stackoverflow.com/questions/tagged/memgraphdb">Stack Overflow</ExternalLink></li>
+                <li><ExternalLink href="https://twitter.com/memgraphdb">Twitter</ExternalLink></li>
+            </ul>
         </div>
-      </div>
-      <div style={{ textAlign: 'center', marginTop: '20px' }}>&copy; 2023 Memgraph. All rights reserved.</div>
+        <div>
+            <ul className="column-text">
+                <li><span className="column-title">More</span ></li>
+                <li><ExternalLink href="https://cloud.memgraph.com/login">Memgraph Cloud</ExternalLink></li>
+                <li><ExternalLink href="https://playground.memgraph.com">Memgraph Playground</ExternalLink></li>
+                <li><ExternalLink href="https://github.com/memgraph/memgraph">GitHub</ExternalLink></li>
+                <li><ExternalLink href="https://www.youtube.com/channel/UCZ3HOJvHGxtQ_JHxOselBYg">YouTube</ExternalLink></li>
+            </ul>
+        </div>
     </footer>
-  );
-};
+);
 
 export default Footer;
