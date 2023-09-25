d2f39a155d4ea36594125211aa273949b8e409cf
Vlasta <95473291+vpavicic@users.noreply.github.com>
2023-09-25 12:49:31 +0200

    [main< ] Improve storage memory (#208)
    * improving-storage-memory
    
    * link from min req
    
    * icons-CL
    
    * monitoring-server
    
    * ldap
    
    * more-ldap
    
    * ldap-finished
    
    * enterprise-features
    
    * performance
    
    * new-sitemap
    
    * latex
    
    * Update package.json
    
    * Update sitemap
    
    ---------
    
    Co-authored-by: kgolubic <kgolubic@gmail.com>
    Co-authored-by: Kruno Golubic <46486712+kgolubic@users.noreply.github.com>

---
+++ components/icons/Rust.js
@@ -0,0 +1,54 @@
+import React from 'react';
+
+const Rust = () => (
+  <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <g clip-path="url(#clip0_977_2410)">
+    <path d="M10.1667 8.87255H12.9706C14.6961 8.87255 14.6961 10.598 12.9706 10.598H10.1667V8.87255ZM3.48039 16.8529H12.1078V14.4804H10.1667V12.7549H12.3235C14.6961 12.7549 13.402 16.8529 15.3431 16.8529H20.7353V12.7549H19.4412V13.1863C19.4412 14.9118 17.5 14.6961 17.2843 13.6176C17.0686 12.5392 16.2059 11.6765 15.9902 11.6765C19.2255 9.95098 17.2843 6.5 14.6961 6.5H4.55882V8.87255H6.71569V14.4804H3.48039V16.8529Z" fill="#646265"/>
+    <path d="M12 21.2745C17.1222 21.2745 21.2745 17.1221 21.2745 12C21.2745 6.8778 17.1222 2.72546 12 2.72546C6.87783 2.72546 2.72549 6.8778 2.72549 12C2.72549 17.1221 6.87783 21.2745 12 21.2745Z" stroke="#646265"/>
+    <path d="M21.9216 12.647L23 12L21.9216 11.3529V12.647Z" fill="#646265" stroke="#646265" stroke-linejoin="round"/>
+    <path d="M21.6047 14.5702L22.7886 14.1459L21.8572 13.3009L21.6047 14.5702Z" fill="#646265" stroke="#646265" stroke-linejoin="round"/>
+    <path d="M20.9187 16.3946L22.1627 16.2095L21.414 15.199L20.9187 16.3946Z" fill="#646265" stroke="#646265" stroke-linejoin="round"/>
+    <path d="M19.89 18.0501L21.1462 18.1113L20.609 16.9741L19.89 18.0501Z" fill="#646265" stroke="#646265" stroke-linejoin="round"/>
+    <path d="M18.5581 19.4731L19.7782 19.7781L19.4731 18.558L18.5581 19.4731Z" fill="#646265" stroke="#646265" stroke-linejoin="round"/>
+    <path d="M16.9741 20.6089L18.1113 21.1461L18.0501 19.8899L16.9741 20.6089Z" fill="#646265" stroke="#646265" stroke-linejoin="round"/>
+    <path d="M15.199 21.4139L16.2095 22.1627L16.3946 20.9187L15.199 21.4139Z" fill="#646265" stroke="#646265" stroke-linejoin="round"/>
+    <path d="M13.301 21.8572L14.146 22.7887L14.5702 21.6047L13.301 21.8572Z" fill="#646265" stroke="#646265" stroke-linejoin="round"/>
+    <path d="M11.3529 21.9216L12 23.0001L12.6471 21.9216H11.3529Z" fill="#646265" stroke="#646265" stroke-linejoin="round"/>
+    <path d="M9.42978 21.6047L9.85401 22.7887L10.699 21.8572L9.42978 21.6047Z" fill="#646265" stroke="#646265" stroke-linejoin="round"/>
+    <path d="M7.60538 20.9187L7.79049 22.1627L8.80099 21.4139L7.60538 20.9187Z" fill="#646265" stroke="#646265" stroke-linejoin="round"/>
+    <path d="M5.94986 19.8899L5.88872 21.1461L7.02588 20.6089L5.94986 19.8899Z" fill="#646265" stroke="#646265" stroke-linejoin="round"/>
+    <path d="M4.52685 18.5581L4.22183 19.7782L5.44193 19.4731L4.52685 18.5581Z" fill="#646265" stroke="#646265" stroke-linejoin="round"/>
+    <path d="M3.39103 16.9742L2.85383 18.1113L4.11 18.0502L3.39103 16.9742Z" fill="#646265" stroke="#646265" stroke-linejoin="round"/>
+    <path d="M2.58605 15.199L1.83732 16.2095L3.08128 16.3947L2.58605 15.199Z" fill="#646265" stroke="#646265" stroke-linejoin="round"/>
+    <path d="M2.14284 13.3009L1.21136 14.146L2.39531 14.5702L2.14284 13.3009Z" fill="#646265" stroke="#646265" stroke-linejoin="round"/>
+    <path d="M2.07843 11.353L0.999999 12L2.07843 12.6471V11.353Z" fill="#646265" stroke="#646265" stroke-linejoin="round"/>
+    <path d="M2.39531 9.42985L1.21136 9.85408L2.14284 10.6991L2.39531 9.42985Z" fill="#646265" stroke="#646265" stroke-linejoin="round"/>
+    <path d="M3.08128 7.60542L1.83732 7.79052L2.58604 8.80103L3.08128 7.60542Z" fill="#646265" stroke="#646265" stroke-linejoin="round"/>
+    <path d="M4.11 5.94986L2.85383 5.88872L3.39103 7.02588L4.11 5.94986Z" fill="#646265" stroke="#646265" stroke-linejoin="round"/>
+    <path d="M5.44193 4.52682L4.22182 4.22179L4.52685 5.44189L5.44193 4.52682Z" fill="#646265" stroke="#646265" stroke-linejoin="round"/>
+    <path d="M7.02588 3.39101L5.88872 2.85382L5.94986 4.10999L7.02588 3.39101Z" fill="#646265" stroke="#646265" stroke-linejoin="round"/>
+    <path d="M8.80099 2.58606L7.79048 1.83734L7.60538 3.0813L8.80099 2.58606Z" fill="#646265" stroke="#646265" stroke-linejoin="round"/>
+    <path d="M10.699 2.14279L9.85401 1.21132L9.42977 2.39526L10.699 2.14279Z" fill="#646265" stroke="#646265" stroke-linejoin="round"/>
+    <path d="M12.6471 2.07849L12 1.00006L11.3529 2.07849L12.6471 2.07849Z" fill="#646265" stroke="#646265" stroke-linejoin="round"/>
+    <path d="M14.5702 2.39529L14.146 1.21135L13.301 2.14282L14.5702 2.39529Z" fill="#646265" stroke="#646265" stroke-linejoin="round"/>
+    <path d="M16.3946 3.0813L16.2095 1.83734L15.199 2.58606L16.3946 3.0813Z" fill="#646265" stroke="#646265" stroke-linejoin="round"/>
+    <path d="M18.0501 4.11009L18.1113 2.85392L16.9741 3.39111L18.0501 4.11009Z" fill="#646265" stroke="#646265" stroke-linejoin="round"/>
+    <path d="M19.4732 5.44193L19.7782 4.22183L18.5581 4.52686L19.4732 5.44193Z" fill="#646265" stroke="#646265" stroke-linejoin="round"/>
+    <path d="M20.609 7.02585L21.1462 5.88869L19.89 5.94983L20.609 7.02585Z" fill="#646265" stroke="#646265" stroke-linejoin="round"/>
+    <path d="M21.414 8.80096L22.1627 7.79045L20.9187 7.60535L21.414 8.80096Z" fill="#646265" stroke="#646265" stroke-linejoin="round"/>
+    <path d="M21.8572 10.6991L22.7886 9.85404L21.6047 9.42981L21.8572 10.6991Z" fill="#646265" stroke="#646265" stroke-linejoin="round"/>
+    <path d="M10.4902 2.94116L12 4.45097L13.5098 2.94116H10.4902Z" stroke="#646265" stroke-width="0.8" stroke-linejoin="round"/>
+    <path d="M20.1489 7.76465L19.1795 9.66711L21.082 10.6365L20.1489 7.76465Z" stroke="#646265" stroke-width="0.8" stroke-linejoin="round"/>
+    <path d="M18.5461 18.4413L16.4372 18.1073L16.1032 20.2162L18.5461 18.4413Z" stroke="#646265" stroke-width="0.8" stroke-linejoin="round"/>
+    <path d="M7.89682 20.2162L7.5628 18.1073L5.45391 18.4413L7.89682 20.2162Z" stroke="#646265" stroke-width="0.8" stroke-linejoin="round"/>
+    <path d="M2.91799 10.6366L4.82046 9.66724L3.8511 7.76478L2.91799 10.6366Z" stroke="#646265" stroke-width="0.8" stroke-linejoin="round"/>
+    </g>
+    <defs>
+    <clipPath id="clip0_977_2410">
+    <rect width="24" height="24" fill="white"/>
+    </clipPath>
+    </defs>
+  </svg>
+);
+
+export default Rust;
