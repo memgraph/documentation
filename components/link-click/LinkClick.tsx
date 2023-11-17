23b4a0bdb69014f0e6cafc837ad32d5df66187c6
Katarina Supe <61758502+katarinasupe@users.noreply.github.com>
2023-11-17 17:47:27 +0100

    Add docker copy custom component (#327)
    * Add docker copy custom component
    
    * update icons and margin
    
    * update change based on theme
    
    * Update code
    
    * Add LinkClick component
    
    * update for dark mode
    
    * Update code
    
    * Update pages/getting-started/install-memgraph/rpm-package.mdx
    
    Co-authored-by: Katarina Supe <61758502+katarinasupe@users.noreply.github.com>
    
    * Update
    
    ---------
    
    Co-authored-by: kgolubic <kgolubic@gmail.com>
    Co-authored-by: Kruno Golubic <46486712+kgolubic@users.noreply.github.com>

---
+++ components/link-click/LinkClick.tsx
@@ -0,0 +1,22 @@
+"use client";
+
+declare global {
+  interface Window {
+    analytics: any;
+  }
+}
+
+export default function LinkClick(props: { link: string, os: string, distribution: string }) {
+
+
+  const handleClick = () => {
+    global.window.analytics.track("Direct download opened", {
+      source: "docs",
+      os: props.os,
+      distribution: props.distribution,
+    });
+  };
+  return (
+      <a href={props.link} onClick={handleClick} className="border-b-[1px] border-[#8c0082] rounded-none dark:border-b-0">{props.link}</a>
+  );
+}
