58c23870f16c272fdbcc5acd14d74e5f1b9eab4b
katarinasupe <supe.katarina@gmail.com>
2025-02-26 17:04:13 +0100

    update kg guide

--- pages/data-modeling/modeling-guides/_meta.ts
+++ pages/data-modeling/modeling-guides/_meta.ts
@@ -1,4 +1,4 @@
 export default {
-    "model-knowledge-graphs": "Model knowledge graphs",
+    "model-a-knowledge-graph": "Model a knowledge graph",
     "model-a-graph-from-csv-file": "Model a graph from CSV file",
 }
