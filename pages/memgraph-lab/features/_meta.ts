1f607456b5207016f469a6bf3f3ac2e055b1c0c7
katarinasupe <supe.katarina@gmail.com>
2025-04-03 16:22:30 +0200

    Add run history

--- pages/memgraph-lab/features/_meta.ts
+++ pages/memgraph-lab/features/_meta.ts
@@ -4,6 +4,7 @@ export default {
     "graph-style-script": "Graph Style Script (GSS)",
     "layout": "Layout",
     "monitoring": "Monitoring",
+    "run-history": "Run history",
     "sharing-features": "Sharing features",
     "single-sign-on": "Single sign-on",
   }
