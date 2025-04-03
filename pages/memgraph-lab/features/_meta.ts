e0ab4eb18df98eb909ee2047cf47949ccf5fe62b
Katarina Supe <61758502+katarinasupe@users.noreply.github.com>
2025-04-03 15:30:38 +0200

    Add layout feature (#1219)

--- pages/memgraph-lab/features/_meta.ts
+++ pages/memgraph-lab/features/_meta.ts
@@ -2,6 +2,7 @@ export default {
     "collections": "Collections",
     "graphchat": "Graph Chat",
     "graph-style-script": "Graph Style Script (GSS)",
+    "layout": "Layout",
     "monitoring": "Monitoring",
     "sharing-features": "Sharing features",
     "single-sign-on": "Single sign-on",
