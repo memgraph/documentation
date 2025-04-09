c0feb6ddda7946c1e54bc5c45d1da8955395a6dd
katarinasupe <supe.katarina@gmail.com>
2025-04-09 13:46:26 +0200

    update gss

--- pages/memgraph-lab/features/graph-style-script/_meta.ts
+++ pages/memgraph-lab/features/graph-style-script/_meta.ts
@@ -1,4 +1,6 @@
 export default {
+  "main-building-blocks": "Main building blocks",
   "built-in-elements": "Built-in elements",
-  "directive-properties": "Directive properties"
+  "directive-properties": "Directive properties",
+  "style-your-graphs-in-memgraph-lab": "Tutorial"
 }
