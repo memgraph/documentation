2a3aa4e0d4aa2461a9413c16fc353e638ef9b17d
Josip Mrden <josip.mrden@memgraph.io>
2025-04-17 14:21:21 +0200

    Remove empty construction pages

--- pages/memgraph-in-production/_meta.ts
+++ pages/memgraph-in-production/_meta.ts
@@ -1,12 +1,3 @@
 export default {
-  "overview": "Overview",
   "general-suggestions": "General suggestions",
-  "memgraph-in-transactional-workloads": "Memgraph in transactional workloads",
-  "memgraph-in-analytical-workloads": "Memgraph in analytical workloads",
-  "memgraph-in-high-throughput-workloads": "Memgraph in high throughput workloads",
-  "memgraph-in-graphrag": "Memgraph in GraphRAG",
-  "memgraph-in-fraud-detection": "Memgraph in Fraud Detection",
-  "memgraph-in-cyber-security": "Memgraph in Cyber Security",
-  "memgraph-in-supply-chain": "Memgraph in Supply Chain",
-  "memgraph-in-mission-critical-workloads": "Memgraph in Mission Critical workloads",
 }
