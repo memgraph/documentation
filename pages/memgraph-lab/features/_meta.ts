64c7a1c9a5454ced0b43d843c392e0ccadda35e5
Toni <toni.lastre@memgraph.io>
2025-04-22 14:30:10 +0200

    New/add lab config docs (#1255)
    * new: Add Lab docs for custom configuration
    
    * Apply suggestions from code review
    
    * update structure
    
    ---------
    
    Co-authored-by: Matea Pesic <80577904+matea16@users.noreply.github.com>
    Co-authored-by: matea16 <mateapesic@hotmail.com>

--- pages/memgraph-lab/features/_meta.ts
+++ pages/memgraph-lab/features/_meta.ts
@@ -1,6 +1,7 @@
 export default {
     "collections": "Collections",
     "csv-file-import": "CSV file import",
+    "custom-configuration": "Custom configuration",
     "graphchat": "GraphChat",
     "graph-schema": "Graph schema",
     "graph-style-script": "Graph Style Script (GSS)",
