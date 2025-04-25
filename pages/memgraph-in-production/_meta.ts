ae10ec9456c182a206084fa64e7334d12880b83a
Josip Mrden <josip.mrden@memgraph.io>
2025-04-25 17:03:09 +0200

    Add contents before the deep-dive

--- pages/memgraph-in-production/_meta.ts
+++ pages/memgraph-in-production/_meta.ts
@@ -2,4 +2,5 @@ export default {
   "general-suggestions": "General suggestions",
   "memgraph-in-graphrag": "Memgraph in GraphRAG use cases",
   "benchmarking-memgraph": "Benchmarking Memgraph",
+  "memgraph-in-high-throughput-workloads": "Memgraph in high-throughput workloads",
 }
