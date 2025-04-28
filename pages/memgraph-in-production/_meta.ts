0ef77f490d1addb45557021d079342d32f7a4760
Josip Mrden <josip.mrden@memgraph.io>
2025-04-28 15:38:57 +0200

    Address PR commnets

--- pages/memgraph-in-production/_meta.ts
+++ pages/memgraph-in-production/_meta.ts
@@ -1,6 +1,6 @@
 export default {
   "general-suggestions": "General suggestions",
   "memgraph-in-graphrag": "Memgraph in GraphRAG use cases",
-  "benchmarking-memgraph": "Benchmarking Memgraph",
   "memgraph-in-high-throughput-workloads": "Memgraph in high-throughput workloads",
+  "benchmarking-memgraph": "Benchmarking Memgraph",
 }
