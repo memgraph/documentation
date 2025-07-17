136a0d0c0e7eaab5331adbd6f456ca7840464925
Andreja Tonev <andreja.tonev@memgraph.io>
2025-07-17 11:06:51 +0200

    WIP

--- pages/database-management/authentication-and-authorization/_meta.ts
+++ pages/database-management/authentication-and-authorization/_meta.ts
@@ -1,7 +1,7 @@
 export default {
     "users": "Users",
     "role-based-access-control": "Role-based access control",
-    "multiple-roles": "Multiple roles per user",
+    "multiple-roles": "Multiple roles per user and multi-tenant roles",
     "auth-system-integrations": "Auth system integrations",
     "impersonate-user": "Impersonate user"
 }
