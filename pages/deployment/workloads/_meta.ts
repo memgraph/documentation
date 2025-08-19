a30b09cb227942c7a2afa26cc5631295cf96cfdc
Memgraph <josip.mrden@memgraph.io>
2025-08-19 13:37:19 +0200

    Add fraud detection blueprint example

--- pages/deployment/workloads/_meta.ts
+++ pages/deployment/workloads/_meta.ts
@@ -4,4 +4,5 @@ export default {
   "memgraph-in-high-throughput-workloads": "Memgraph in high-throughput workloads",
   "memgraph-in-mission-critical-workloads": "Memgraph in mission critical workloads",
   "memgraph-in-supply-chain": "Memgraph in supply chain",
+  "memgraph-in-fraud-detection": "Memgraph in fraud detection",
 }
