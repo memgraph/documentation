2a23a9feada9f333df03507ec698ee155b6ebbd7
andrejtonev <29177572+andrejtonev@users.noreply.github.com>
2025-08-22 14:44:44 +0200

    Multi-role user with ability to link roles to specific databases (#1337)
    * WIP
    
    * WIP
    
    * env var
    
    * auth database
    
    * repl/mt database
    
    * SHOW ROLES/PRIVILEGES ON
    
    * mt fixes
    
    * details
    
    * Apply suggestions from code review
    
    Co-authored-by: Matea Pesic <80577904+matea16@users.noreply.github.com>
    
    * recent changes -> as of v3.5
    
    * typo
    
    * cleanup
    
    * cleanup
    
    * cleanup multi-role
    
    * cleanup
    
    * enterprise page
    
    * apply review changes
    
    ---------
    
    Co-authored-by: Matea Pesic <80577904+matea16@users.noreply.github.com>
    Co-authored-by: matea16 <mateapesic@hotmail.com>

--- pages/database-management/authentication-and-authorization/_meta.ts
+++ pages/database-management/authentication-and-authorization/_meta.ts
@@ -1,6 +1,7 @@
 export default {
     "users": "Users",
     "role-based-access-control": "Role-based access control",
+    "multiple-roles": "Multiple roles per user and multi-tenant roles",
     "auth-system-integrations": "Auth system integrations",
     "impersonate-user": "Impersonate user",
     "user-profiles": "User profiles"
