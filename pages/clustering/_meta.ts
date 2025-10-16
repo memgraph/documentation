90fe9a06348dbaa90ea90c1a013fc07f8880b442
Josip Mrđen <josip.mrden@memgraph.io>
2025-10-16 15:52:48 +0200

    Add more pages to the FAQ

--- pages/clustering/_meta.ts
+++ pages/clustering/_meta.ts
@@ -1,6 +1,7 @@
 export default {
     "high-availability": "High availability",
+    "bolt-routing": "Bolt+routing protocol",
+    "ha-commands-reference": "High availability reference queries",
     "replication": "Replication",
     "faq": "FAQ"
 }
-  
