cd362bd684e6a567a3c15e577dff4a0cd1a41084
Josip Mrđen <josip.mrden@memgraph.io>
2025-10-29 16:00:07 +0100

    Partial how HA works

---
+++ pages/clustering/concepts/_meta.ts
@@ -0,0 +1,4 @@
+export default {
+    "how-replication-works": "How replication works",
+    "how-high-availability-works": "How high availability works",
+}
---
+++ pages/clustering/high-availability/_meta.ts
@@ -0,0 +1,8 @@
+export default {
+    "setup-ha-cluster-docker": "Setup HA cluster with Docker",
+    "setup-ha-cluster-docker-compose": "Setup HA cluster with Docker Compose",
+    "setup-ha-cluster-k8s": "Setup HA cluster with K8s",
+    "querying-the-cluster": "Querying the cluster",
+    "best-practices": "Best practices",
+    "ha-commands-reference": "Reference commands",
+}
