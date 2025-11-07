c4fc43009afd8d7c2e75b0dfb66b3ff243282397
Colin Barry <colin.barry@memgraph.io>
2025-11-07 15:02:47 +0000

    docs: Add migration to v3.7 LBAC guide

--- pages/database-management/authentication-and-authorization/_meta.ts
+++ pages/database-management/authentication-and-authorization/_meta.ts
@@ -1,6 +1,7 @@
 export default {
     "users": "Users",
     "role-based-access-control": "Role-based access control",
+    "mlbac-migration-guide": "Migrating to v3.7 LBAC",
     "multiple-roles": "Multiple roles per user and multi-tenant roles",
     "auth-system-integrations": "Auth system integrations",
     "impersonate-user": "Impersonate user",
