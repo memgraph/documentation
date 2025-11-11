2867954bf3824d1e49c5cebac45208d33a583351
Josip Mrđen <josip.mrden@memgraph.io>
2025-11-11 16:36:48 +0100

    Add general guidance on HA

--- pages/clustering/concepts/_meta.ts
+++ pages/clustering/concepts/_meta.ts
@@ -1,4 +1,5 @@
 export default {
     "how-replication-works": "How replication works",
     "how-high-availability-works": "How high availability works",
+    "querying-the-cluster-in-high-availability": "Querying the cluster in HA"
 }
