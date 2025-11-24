6d2c26decfbf2c936d2bdae96828ed85070f14b3
Josip Mrđen <josip.mrden@memgraph.io>
2025-11-24 10:27:34 +0100

    Multi-tenancy revamp

---
+++ pages/database-management/multi-tenancy/_meta.ts
@@ -0,0 +1,7 @@
+export default {
+  "how-multi-tenancy-works": "How multi-tenancy works",
+  "setup-multi-tenancy": "Setup multi-tenancy",
+  "best-practices": "Best practices",
+  "reference-commands": "Reference commands",
+  "faq": "FAQ"
+}
