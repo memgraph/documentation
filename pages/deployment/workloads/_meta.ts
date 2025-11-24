708fac687538978ac1decad286b0bc05e7330208
Josipmrden <josip.mrden@memgraph.io>
2025-11-24 11:04:58 +0000

    Add memgraph in supply chain workload (#1366)
    * init
    
    * Add hardware sizing
    
    * Add vm.max_map_count explanation
    
    * Add deployment options
    
    * Add flag set suggestions
    
    * Indentation
    
    * Remove unnecessary comment
    
    * Add enterprise, queries and import sections
    
    * Finish general suggestions guide
    
    * Make under construction notes
    
    * Add todo
    
    * Update property sizes
    
    * Add backup considerations
    
    * Added overview page
    
    * Add hardware sizing
    
    * Add vm.max_map_count explanation
    
    * Add deployment options
    
    * Add flag set suggestions
    
    * Indentation
    
    * Remove unnecessary comment
    
    * Add enterprise, queries and import sections
    
    * Finish general suggestions guide
    
    * Make under construction notes
    
    * Add todo
    
    * Update property sizes
    
    * Add backup considerations
    
    * Added overview page
    
    * Main
    
    * Main
    
    * Newline
    
    * Add section for query timeout
    
    * Set up lab features
    
    * Sentence case
    
    * Remove empty construction pages
    
    * Add callout
    
    * Update GraphRAG use case
    
    * Finish memgraph in production for graphrag
    
    * Update initial page
    
    * Add graphrag link
    
    * Add page for evaluating memgraph -> mgbench
    
    * Address PR comments
    
    * Add title for evaluating memgraph
    
    * Address PR comments
    
    * Add contents before the deep-dive
    
    * Edit titles
    
    * Add guidelines for updating the page
    
    * Finish guide for high throughput workloads
    
    * Omit constructed guide
    
    * Add initial mission critical page
    
    * Omit guide being constructed
    
    * Dash
    
    * Address PR commnets
    
    * Push memgraph up
    
    * Fix link for benchmarking Memgraph
    
    * Add guide for mission critical workloads
    
    * Link backup and restore
    
    * Add memgraph in supply chain workload
    
    * Apply suggestions from code review
    
    * Address PR comments
    
    ---------
    
    Co-authored-by: katarinasupe <supe.katarina@gmail.com>
    Co-authored-by: Katarina Supe <61758502+katarinasupe@users.noreply.github.com>
    Co-authored-by: Matea Pesic <80577904+matea16@users.noreply.github.com>

--- pages/deployment/workloads/_meta.ts
+++ pages/deployment/workloads/_meta.ts
@@ -3,4 +3,5 @@ export default {
   "memgraph-in-graphrag": "Memgraph in GraphRAG use cases",
   "memgraph-in-high-throughput-workloads": "Memgraph in high-throughput workloads",
   "memgraph-in-mission-critical-workloads": "Memgraph in mission critical workloads",
+  "memgraph-in-supply-chain": "Memgraph in supply chain",
 }
