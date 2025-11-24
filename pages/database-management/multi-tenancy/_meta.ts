bc499a73ca77dd4f9c44435e4bcd4714118b0b63
Josip Mrđen <josip.mrden@memgraph.io>
2025-11-24 15:02:13 +0100

    Reference commands refactor

--- pages/database-management/multi-tenancy/_meta.ts
+++ pages/database-management/multi-tenancy/_meta.ts
@@ -1,6 +1,6 @@
 export default {
   "how-multi-tenancy-works": "How multi-tenancy works",
-  "setup-multi-tenancy": "Setup multi-tenancy",
+  "setup-examples": "Setup examples",
   "best-practices": "Best practices",
   "reference-commands": "Reference commands",
   "faq": "FAQ"
