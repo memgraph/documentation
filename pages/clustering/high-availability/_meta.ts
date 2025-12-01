e179f27e47706f2d095c550b9ca4339ad6d3ebc8
matea16 <mateapesic@hotmail.com>
2025-12-01 15:29:11 +0100

    update under the hood

--- pages/clustering/high-availability/_meta.ts
+++ pages/clustering/high-availability/_meta.ts
@@ -1,9 +1,9 @@
 export default {
     "how-high-availability-works": "Under the hood",
     "querying-the-cluster-in-high-availability": "Querying the cluster in HA",
-    "setup-ha-cluster-docker": "Setup HA cluster with Docker",
-    "setup-ha-cluster-docker-compose": "Setup HA cluster with Docker Compose",
-    "setup-ha-cluster-k8s": "Setup HA cluster with K8s",
+    "setup-ha-cluster-docker": "Set up HA cluster with Docker",
+    "setup-ha-cluster-docker-compose": "Set up HA cluster with Docker Compose",
+    "setup-ha-cluster-k8s": "Set up HA cluster with K8s",
     "best-practices": "Best practices",
     "ha-commands-reference": "Reference commands",
 }
