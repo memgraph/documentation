a945ce49449ffcb95d00eca493d8ce5e7e669e68
Marko Budiselic <marko.budiselic@memgraph.com>
2026-03-29 17:14:54 +0200

    Add MemGQL under Preview

---
+++ pages/preview/memgql/_meta.ts
@@ -0,0 +1,7 @@
+export default {
+  "quick-start": "Quick Start",
+  "features": "Features",
+  "connect": "Connect",
+  "config": "Configuration",
+  "licensing": "Licensing",
+}
---
+++ pages/preview/memgql/connect/_meta.ts
@@ -0,0 +1,7 @@
+export default {
+  "duckdb": "to DuckDB",
+  "iceberg": "to Iceberg",
+  "memgraph": "to Memgraph",
+  "neo4j": "to Neo4j",
+  "postgres": "to PostgreSQL",
+}
