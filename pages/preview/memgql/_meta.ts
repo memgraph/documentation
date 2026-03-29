9ec0d6295cc96178f6bbfc1cf33595d43eaf48f5
Marko Budiselić <marko.budiselic@memgraph.com>
2026-03-29 18:29:06 +0200

    Add MemGQL docs pages (under Preview) (#1574)

---
+++ pages/preview/memgql/_meta.ts
@@ -0,0 +1,8 @@
+export default {
+  "quick-start": "Quick Start",
+  "features": "Features",
+  "connect": "Connect",
+  "config": "Configuration",
+  "changelog": "Changelog",
+  "licensing": "Licensing",
+}
