53ba6cbd44fd22ce1c659a6de09d41b84ab2530a
as51340 <andi8647@gmail.com>
2026-08-04 12:39:38 +0200

    feat: SSO coords

--- pages/clustering/high-availability/_meta.ts
+++ pages/clustering/high-availability/_meta.ts
@@ -1,6 +1,7 @@
 export default {
     "how-high-availability-works": "Under the hood",
     "querying-the-cluster-in-high-availability": "Querying the cluster in HA",
+    "coordinator-authentication": "Coordinator authentication",
     "setup-ha-cluster-docker": "Set up HA cluster with Docker",
     "setup-ha-cluster-docker-compose": "Set up HA cluster with Docker Compose",
     "setup-ha-cluster-k8s": "Set up HA cluster with K8s",
